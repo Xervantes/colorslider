64986a9d306cf63eca57348f4145f472aef21c9f
bxc5103 <benjamin_cervantes@homedepot.com>
2019-02-20 09:46:42 -0500

    added 3 components and outline of the components

---
+++ src/components/ColorOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ColorOutput = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ColorOutput;
---
+++ src/components/ColorSlider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ColorSlider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ColorSlider;
