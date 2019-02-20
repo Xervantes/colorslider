1803d44eef4f828bb5f0ac70f3d80a242d33c434
bxc5103 <benjamin_cervantes@homedepot.com>
2019-02-20 11:58:58 -0500

    dynamically displaying color in output field and changing color of text

--- src/components/ColorOutput.js
+++ src/components/ColorOutput.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './ColorBrowser.css';
 
 const ColorOutput = (props) => {
+
+    const colorSum = Number(props.Red) + Number(props.Green) + Number(props.Blue);
+    const textColor = (colorSum > 128*3) ? `black` : `white`;
+    const rgbString = `rgb(${props.Red}, ${props.Green}, ${props.Blue})`;
+    const theStyle = {
+        backgroundColor: rgbString,
+        color: textColor
+    };
+
+
     return (
-        <div className='color-output'>
+        <div className='color-output' style={theStyle}>
             <h2>RGB: {props.Red}, {props.Green}, {props.Blue}</h2>
         </div>
     );
